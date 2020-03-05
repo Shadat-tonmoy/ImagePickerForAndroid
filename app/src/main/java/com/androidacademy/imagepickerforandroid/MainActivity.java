a232d1fd266877c5dc52619419f0c0c0aeacd696
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-03-05 19:10:53 +0600

    initial commit

---
+++ app/src/main/java/com/androidacademy/imagepickerforandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.androidacademy.imagepickerforandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
